b8a312b30edcd163fc4087b0779dce763a805359
thejaduijin <lokendra.06.work@gmail.com>
2022-12-06 15:28:42 +0530

    ui is done

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -4,25 +4,61 @@ import '../styles.css';
 function Folder({ explorer }) {
     console.log(explorer);
 
-    const [expand,setExpand] = useState(false);
+    const [expand, setExpand] = useState(false);
+    const [showInput, setShowInput] = useState({
+        visible: false,
+        isFolder: null
+    });
 
-    if (explorer.isFolder){
+    const handleNewFolder = (e, isFolder) => {
+        e.stopPropagation();
+        setShowInput({
+            visible: true,
+            isFolder
+        })
+    }
+
+    const onAddFolder = (e) => {
+        if (e.keyCode === 13 && e.target.value){
+            setShowInput({...showInput,visible:false})
+        }
+    }
+
+    if (explorer.isFolder) {
         return (
             <div style={{ margin: 5 }}>
-                <div className='folder' onClick={()=>setExpand(!expand)}>
+                <div className='folder' onClick={() => setExpand(!expand)}>
                     <span>
                         📁 {explorer.name}
                     </span>
+                    <div>
+                        <button onClick={(e) => handleNewFolder(e, true)}>Add Folder</button>
+                        <button onClick={(e) => handleNewFolder(e, false)}>Add File</button>
+                    </div>
                 </div>
-                <div style={{display:expand?"block":"none", paddingLeft:25}}>
+                <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+                    {
+                        showInput.visible && (
+                            <div className='inputContainer'>
+                                <span>{showInput.isFolder ? "📁" : "📄"}</span>
+                                <input
+                                    type="text"
+                                    onKeyDown={onAddFolder}
+                                    onBlur={() => setShowInput({ ...showInput, visible: false })}
+                                    className='inputContainer__input'
+                                    autoFocus />
+                            </div>
+                        )
+                    }
                     {explorer.items.map((exp) => {
                         return (
-                           <Folder explorer={exp} key={exp.id}/>
+                            <Folder explorer={exp} key={exp.id} />
                         )
                     })}
                 </div>
             </div>
-        )} else {
+        )
+    } else {
         return <span className='file'>📄 {explorer.name}</span>
     }
 }
