d100e162e855cf5105aa7e4f9dd931e15f491bfd
thejaduijin <lokendra.06.work@gmail.com>
2022-12-06 13:43:38 +0530

    first level is ready

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Folder({explorer}) {
+function Folder({ explorer }) {
     console.log(explorer);
-  return (
-    <div style={{margin:5}}>
-      <div>
-        📁 {explorer.name}
-      </div>
-    </div>
-  )
+
+    const [expand,setExpand] = useState(false);
+
+    if (explorer.isFolder){
+        return (
+            <div style={{ margin: 5 }}>
+                <div className='folder' onClick={()=>setExpand(!expand)}>
+                    <span>
+                        📁 {explorer.name}
+                    </span>
+                </div>
+                <div style={{display:expand?"block":"none"}}>
+                    {explorer.items.map((exp) => {
+                        return (
+                            <span>
+                                {exp.name}
+                            </span>
+                        )
+                    })}
+                </div>
+            </div>
+        )} else {
+        return <span className='file'>📄 {explorer.name}</span>
+    }
 }
 
 export default Folder;
